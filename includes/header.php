bf899f81cbf47f6beffec9986c119cfcacc7f2b4
Jerem <jer.ra76@laposte.net>
2019-12-13 16:44:49 +0100

    oui

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,22 @@
-<header>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 
-  <h1>Le site pour le 10 janvier</h1>
-  <nav>
-    <ul>
-      <li> <a href="index.php?page=accueil" title="Retour à la page d'acceuil">Acceuil </a> </li>
-      <li> <a href="index.php?page=cv" title="Mon CV">CV</a> </li>
-      <li> <a href="index.php?page=contact" title="nous contacter">Contact </a> </li>
-      <li> <a href="index.php?page=mentions" title="Les mentions légales du site">Mentions légales</a> </li>
-    </ul>
-  </nav>
-</header>
+
+      <h1>Le site pour le 10 janvier</h1>
+      <nav>
+        <ul>
+          <li> <a href="index.php?page=accueil" title="Retour à la page d'acceuil">Acceuil </a> </li>
+          <li> <a href="index.php?page=cv" title="Mon CV">CV</a> </li>
+          <li> <a href="index.php?page=contact" title="nous contacter">Contact </a> </li>
+          <li> <a href="index.php?page=mentions" title="Les mentions légales du site">Mentions légales</a> </li>
+        </ul>
+      </nav>
+    
+
+  </body>
+</html>
